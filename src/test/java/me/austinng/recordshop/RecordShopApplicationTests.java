deb2abc537360125e8efe478da225dd334e414ab
tungnguyen <tungnnnt17@gmail.com>
2025-05-12 10:26:50 +0700

    initial commit

---
+++ src/test/java/me/austinng/recordshop/RecordShopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.austinng.recordshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
