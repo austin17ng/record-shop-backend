8360a002215e91a504e65f068f97b2a97de8da11
tungnguyen <tungnnnt17@gmail.com>
2025-05-14 14:48:07 +0700

    P12. Testing

--- src/main/java/me/austinng/recordshop/dto/ArtistDto.java
+++ src/main/java/me/austinng/recordshop/dto/ArtistDto.java
@@ -1,8 +1,10 @@
 package me.austinng.recordshop.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class ArtistDto {
     private Long id;
---
+++ src/test/java/me/austinng/recordshop/controller/ArtistControllerTest.java
@@ -0,0 +1,37 @@
+package me.austinng.recordshop.controller;
+
+import me.austinng.recordshop.dto.ArtistDto;
+import me.austinng.recordshop.dto.ArtistMapper;
+import me.austinng.recordshop.model.Artist;
+import me.austinng.recordshop.repository.ArtistRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ArtistControllerTest {
+    @Mock
+    private ArtistRepository artistRepository;
+
+    @Mock
+    private ArtistMapper artistMapper;
+
+    @InjectMocks
+    private ArtistController artistController;
+
+    @Test
+    public void getAllArtists_returnAllArtists() {
+        List<Artist> artists = List.of(new Artist(0L, "Central Cee", "UK", "Rap"));
+        when(artistRepository.findAll()).thenReturn(artists);
+
+        List<ArtistDto> result = artistController.getAllArtists();
+        assertEquals(1, result.size());
+    }
+}
