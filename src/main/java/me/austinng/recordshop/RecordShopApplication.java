3693b2b7a45c6083a437d771f570f5dcfe1d4b2b
Austin Nguyen <gentlewoolworks@outlook.com>
2025-05-18 16:39:06 +0700

    chore: remove init user in application

--- src/main/java/me/austinng/recordshop/RecordShopApplication.java
+++ src/main/java/me/austinng/recordshop/RecordShopApplication.java
@@ -14,18 +14,4 @@ public class RecordShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecordShopApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner init(UserRepository userRepository, PasswordEncoder encoder) {
-		return args -> {
-			if (userRepository.findByUsername("tung").isEmpty()) {
-				User user = new User();
-				user.setUsername("tung");
-				user.setPassword(encoder.encode("12345"));
-				userRepository.save(user);
-			}
-		};
-	}
-
-
 }
