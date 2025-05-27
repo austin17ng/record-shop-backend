f0409d6f280314ab39aa927e47bf1795422a8902
tungnguyen <tungnnnt17@gmail.com>
2025-05-27 11:25:08 +0700

    fix: order url

--- src/main/java/me/austinng/recordshop/controller/OrderController.java
+++ src/main/java/me/austinng/recordshop/controller/OrderController.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/api/orders")
 public class OrderController {
     @Autowired
     AlbumRepository albumRepository;
